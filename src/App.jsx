bda5c55d45e945dd9e1589f2998f51fb9cc4f9fd
HishamSaeed <hisham.saeed.92@gmail.com>
2024-02-27 19:18:29 +0100

    splitted app into 3 div

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@ import './App.css'
 function App() {
 
   return (
-    <>
-    </>
+    <div className='app'>
+      <div className='navBar'>Nav Bar</div>
+      <div className='layout'>
+        <div>Parameter</div>
+        <div>Visulaziation</div>
+        <div>Simulation Control</div>
+      </div>
+    </div>
   )
 }
 
