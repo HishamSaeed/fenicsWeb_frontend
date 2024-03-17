b009adde71633f9ccab9cd71cd74812af4ad5a07
HishamSaeed <hisham.saeed.92@gmail.com>
2024-03-17 15:07:45 +0100

    added startup file

--- src/services/ApiService.jsx
+++ src/services/ApiService.jsx
@@ -1,6 +1,6 @@
 import io from "socket.io-client"
 
-const apiBaseUrl = 'http://127.0.0.1:5000';
+const apiBaseUrl = 'http://192.168.178.23:5000';
 const socket = io(apiBaseUrl);
 
 export const fetchData = async (endpoint) => {
